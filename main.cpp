356e14c086e7283874a290ceea6ea7fcc3826081
hiddevuijk <hdvuijk@gmail.com>
2018-10-18 15:28:36 +0200

    trivial

--- main.cpp
+++ main.cpp
@@ -21,36 +21,34 @@ int main()
 	topology[1] = 15;
 	topology[2] = 15;
 	topology[3] = 15;
-	topology[4] = 2;
+	topology[4] = 1;
 	NeuralNet net(topology);
 	NeuralNet net2(topology);	
 
 	vector<double> input(1);
 	vector<double> target(1);
-	vector<double> output(2);
+	vector<double> output(1);
 
 	int N = 10;
 
 	vector<vector<double> > inputTrain(N,vector<double>(1));
-	vector<vector<double> > targetTrain(N,vector<double>(2));
+	vector<vector<double> > targetTrain(N,vector<double>(1));
 	vector<double> errorTrain;
 	vector<vector<double> > inputTest(N,vector<double>(1));
-	vector<vector<double> > targetTest(N,vector<double>(2));
+	vector<vector<double> > targetTest(N,vector<double>(1));
 	vector<double> errorTest;
 
 	//training data
 	for(int i =0;i<N;++i){
 		inputTest[i][0] = 4*(1-2*( (double)rand()/(double)RAND_MAX));
 		targetTest[i][0] = sin(inputTest[i][0]);
-		targetTest[i][1] = cos(inputTest[i][0]);
 		inputTrain[i][0] = 4*(1-2*( (double)rand()/(double)RAND_MAX));
 		targetTrain[i][0] = sin(inputTrain[i][0]);
-		targetTrain[i][1] = cos(inputTrain[i][0]);
 	}
 	double eta = 0.1;
 	double eTest = 0.;
 	double eTrain = 0.;
-	for(int i=0;i<2000;++i) {
+	for(int i=0;i<200;++i) {
 		eTrain = 0.;
 		eTest = 0.;
 		for(int j=0;j<N;++j) {
@@ -67,7 +65,6 @@ int main()
 			net.forwardProp(inputTrain[k]);
 			net.backProp(targetTrain[k],eta);
 		}
-		if(i == 1000) eta = 0.01;
 	}
 
 	ofstream outTrain("errorTrain.dat");
@@ -77,24 +74,21 @@ int main()
 		outTrain << errorTrain[i] << '\n';
 		outTest << errorTest[i] << '\n';
 	}
-	ofstream outX("xtrain.dat");
-	ofstream outYs("ytrains.dat");
-	ofstream outYc("ytrainc.dat");
+	ofstream outXtrain("xtrain.dat");
+	ofstream outYtrain("ytrain.dat");
 	ofstream outXtest("xtest.dat");
-	ofstream outYtests("ytests.dat");
-	ofstream outYtestc("ytestc.dat");
+	ofstream outYtest("ytest.dat");
 	for(int i =0;i<N;++i){
 		net.forwardProp(inputTrain[i]);
 		output = net.getOutput();
 
-		outX << inputTrain[i][0] << '\n';
-		outYs << output[0] << '\n';
-		outYc << output[1] << '\n';
+		outXtrain << inputTrain[i][0] << '\n';
+		outYtrain << output[0] << '\n';
 		net.forwardProp(inputTest[i]);
+
 		output = net.getOutput();
 		outXtest << inputTest[i][0] << '\n';
-		outYtests << output[0] << '\n';
-		outYtestc << output[1] << '\n';
+		outYtest << output[0] << '\n';
 
 	}
 
